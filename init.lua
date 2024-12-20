d070db158153338a1edf307f9766ab7c2fb42dc7
Grant Allor <gallor@drwholdings.com>
2024-12-20 11:54:54 -0600

    Remove obsidian dependency for the moment

--- init.lua
+++ init.lua
@@ -775,33 +775,33 @@ require('lazy').setup({
   },
 
   -- Obsidian
-  {
-    'epwalsh/obsidian.nvim',
-    version = '*',
-    lazy = true,
-    event = {
-      'BufReadPre ' .. vim.fn.expand '~' .. '/Documents/Vault/*.md',
-      'BufNewFile ' .. vim.fn.expand '~' .. '/Documents/Vault/*.md',
-    },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'hrsh7th/nvim-cmp',
-      'nvim-telescope/telescope.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    opts = {
-      workspaces = {
-        {
-          name = 'Vault',
-          path = '~/Documents/Vault',
-        },
-      },
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-      },
-    },
-  },
+  -- {
+  --   'epwalsh/obsidian.nvim',
+  --   version = '*',
+  --   lazy = true,
+  --   event = {
+  --     'BufReadPre ' .. vim.fn.expand '~' .. '/Documents/Vault/*.md',
+  --     'BufNewFile ' .. vim.fn.expand '~' .. '/Documents/Vault/*.md',
+  --   },
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'hrsh7th/nvim-cmp',
+  --     'nvim-telescope/telescope.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --   },
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = 'Vault',
+  --         path = '~/Documents/Vault',
+  --       },
+  --     },
+  --     completion = {
+  --       nvim_cmp = true,
+  --       min_chars = 2,
+  --     },
+  --   },
+  -- },
 
   {
     'rcarriga/nvim-notify',
