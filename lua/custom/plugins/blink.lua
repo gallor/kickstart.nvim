4c1e65d9b78d6e7c0f939add236772cf0a22a759
Grant Allor <gallor@drwholdings.com>
2026-07-15 11:12:45 -0400

    Add blink

---
+++ lua/custom/plugins/blink.lua
@@ -0,0 +1,95 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = {
+      -- 'prefix' will fuzzy match on the text before the cursor
+      -- 'full' will fuzzy match on the text before _and_ after the cursor
+      -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+      keyword = { range = 'full' },
+
+      -- Disable auto brackets
+      -- NOTE: some LSPs may add auto brackets themselves anyway
+      accept = { auto_brackets = { enabled = false }, },
+
+      -- Don't select by default, auto insert on selection
+      list = { selection = { preselect = false, auto_insert = true } },
+
+      -- Show documentation when selecting a completion item
+      documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'single' } },
+
+      menu = {
+        -- Disable automatically showing the menu while typing, instead press `<C-space>` (by default) to show it manually
+        auto_show = false,
+        border = 'single',
+        -- or per filetype
+        -- auto_show = function(ctx, items) return vim.bo.filetype == 'markdown' end,
+        -- Delay before showing the completion menu while typing
+        -- auto_show_delay_ms = 500,
+        -- or per filetype
+        -- auto_show_delay_ms = function(ctx, items) return vim.bo.filetype == 'markdown' and 1000 or 0 end,
+        draw = {
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind" }
+          },
+        }
+      }
+    },
+
+    signature = { enabled = true, window = { border = 'single' } },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = {
+      implementation = "prefer_rust_with_warning",
+      sorts = {
+        'exact',
+        'score',
+        'sort_text'
+      }
+    }
+  },
+  opts_extend = { "sources.default" }
+}
